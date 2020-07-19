f49056ddf3f8468a8dc7dd20973fb270eac75f6d
Brillianttyagi <deepanshubhai84@gmail.com>
2020-07-19 23:36:14 +0530

    full app added

---
+++ weatherapp/url.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index,name="index"),
+    path('fetchweather',views.fetchweather,name="fetchweather"),
+    path('', views.index,name="index"),
+]
